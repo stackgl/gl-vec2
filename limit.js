ea3df042bfea35f6e428360126a40e828c7fdac6
alberto <alberto.massa@ymail.com>
2018-08-15 16:55:06 +0200

    removed markdown fro doc

--- limit.js
+++ limit.js
@@ -1,11 +1,11 @@
-module.exports = limit
+module.exports = limit;
 
-var squaredLength = require('./squaredLength');
-var scale = require('./scale');
-var copy = require('./copy');
+var squaredLength = require("./squaredLength");
+var scale = require("./scale");
+var copy = require("./copy");
 
 /**
- * Limit the magnitude of this vector to the value used for the <b>max</b>
+ * Limit the magnitude of this vector to the value used for the `max`
  * parameter.
  *
  * @method limit
@@ -13,17 +13,16 @@ var copy = require('./copy');
  * @param  max the maximum magnitude for the vector
  * @returns out
  */
-
 function limit(out, a, max) {
   var mSq = squaredLength(a);
-  
+
   if (mSq > max * max) {
-    var n = Math.sqrt(mSq)
-    out[0] = a[0] / n
-    out[1] = a[1] / n
+    var n = Math.sqrt(mSq);
+    out[0] = a[0] / n;
+    out[1] = a[1] / n;
     scale(out, out, max);
   } else {
-    copy(out, a)
+    copy(out, a);
   }
 
   return out;
